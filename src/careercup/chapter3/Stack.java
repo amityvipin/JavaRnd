18aa2eed773cc6370ea7854b03463b6778cc9722
vipin kumar <amity.vipin@gmail.com>
2016-07-28 10:27:42 +0530

    Stack Implementation

---
+++ src/careercup/chapter3/Stack.java
@@ -0,0 +1,38 @@
+package careercup.chapter3;
+
+import java.lang.reflect.Array;
+import java.util.stream.Stream;
+
+public class Stack<T extends Comparable<T>> {
+	
+	@SuppressWarnings("unchecked")
+	public Stack(Class<T> clazz, int capacity) {
+        array = (T[])Array.newInstance(clazz, capacity);
+    }
+	int top = 0;
+    private final T[] array;
+	int mininumIndex = -1;
+	public void push(T value){
+		if(mininumIndex==-1 || value.compareTo(array[mininumIndex])<0){
+			mininumIndex = top;
+		}
+		array[top++] = value;
+	}
+	
+	public T pop(){
+		return array[top--];
+	}
+	
+	public T peek(){
+		return array[top];
+	}
+	
+	public T minimum(){
+		return array[mininumIndex];
+	}
+
+	public void print(){
+	 System.out.println("bottom to top ");	
+	 Stream.of(array).forEach(System.out::println);
+	}
+}
---
+++ src/careercup/chapter3/StackMinimum.java
@@ -0,0 +1,17 @@
+package careercup.chapter3;
+
+public class StackMinimum {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>(Integer.class,10);
+		stack.push(2);
+		stack.push(6);
+		stack.push(8);
+		stack.push(3);
+		stack.push(9);
+		stack.push(9);
+		stack.push(5);
+		System.out.println(stack.minimum());
+		stack.print();
+	}
+
+}
